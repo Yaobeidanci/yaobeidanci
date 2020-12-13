0eae215f90239efae4e47d20ec38fe39055dfef9
lamour <cdy15347875599@sina.com>
2020-12-13 17:31:07 +0800

    ddd

--- app/src/main/java/yaobeidanci/view/MainActivity.java
+++ app/src/main/java/yaobeidanci/view/MainActivity.java
@@ -2,21 +2,27 @@ package yaobeidanci.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import yaobeidanci.view.mainpage.SelfPage;
 
 public class MainActivity extends AppCompatActivity {
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
     }
 
     public void onclick(View view){
@@ -25,4 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    public static Context getContext() {
+        return context;
+    }
+
 }
